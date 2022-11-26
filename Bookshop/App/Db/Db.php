f27ddafe2d72f2e5c07790f329667b9787979804
Dinho MaQ <97320346+dinhomaq@users.noreply.github.com>
2022-11-26 16:04:24 +0100

    Update Db.php

--- Bookshop/App/Db/Db.php
+++ Bookshop/App/Db/Db.php
@@ -4,42 +4,39 @@ namespace App\Db;
 use PDO;
 use PDOException;
 
-class Db{
+class Db
+{
 
-    Private static $instance;
+    private static $instance;
 
-    public static function db(){
+    public static function db()
+    {
 
         self::$instance = NULL;
 
-        try{
-            
-            self::$instance = new PDO('mysql:host=localhost;dbname=db_bookshop;charset=utf8','root','');
+        try {
+
+            self::$instance = new PDO('mysql:host=localhost;dbname=db_bookshop;charset=utf8', 'root', '');
 
             self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $erro) {
+
+            echo "Error menssage" . $erro->getMessage();
+            echo "Line Error" . $erro->getLine();
+
+        }
+        if (!self::$instance) {
+
+            echo "erro ao conectar";
+        } else {
+            // echo "conectou";
+            return self::$instance;
         }
 
-        
-
-   catch(PDOException $erro){
-
-    echo "Error menssage". $erro->getMessage();
-    echo "Line Error". $erro->getLine();
-    
-   }
-   if(!self::$instance){
-
-    echo "erro ao conectar";
-}
-else{
-   // echo "conectou";
-   return self::$instance;
-}
-            
-} 
+    }
 }
 
 
 
-            
+
 ?>
